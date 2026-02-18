352aa9a45aba236dbeb21af24c1119afbbef7496
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-19 02:45:49 +0600

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -11,7 +11,7 @@ const variantSchema = new mongoose.Schema(
 const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true },
-
+soldCount: { type: Number, default: 0 },
     category: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Category",
